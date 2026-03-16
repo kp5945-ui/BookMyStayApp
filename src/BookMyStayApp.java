8b9ff70e9733f008421a211914cb5c23073b5490
kp5945-ui <kushalpadala27@gmail.com>
2026-03-16 08:46:17 +0530

    UC1: Application Entry & Welcome Message

--- src/BookMyStayApp.java
+++ src/BookMyStayApp.java
@@ -1,2 +1,22 @@
+
+import java.util.Scanner;
+
 public class BookMyStayApp {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Hotel Booking System!");
+        System.out.println("Application: Book My Stay");
+        System.out.println("Version: 1.0");
+        System.out.println("-----------------------------------");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Please enter your name: ");
+        String userName = scanner.nextLine();
+        System.out.print("Enter number of nights you want to stay: ");
+        int nights = scanner.nextInt();
+        System.out.println("\nThank you, " + userName + "!");
+        System.out.println("You have requested to book " + nights + " night(s).");
+        System.out.println("Our team will process your booking shortly.");
+        scanner.close();
+        System.out.println("\nApplication terminated successfully.");
+    }
 }
